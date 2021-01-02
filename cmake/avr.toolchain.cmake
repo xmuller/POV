48deee4d7c96e8144584bd63d33d40c8ad85a8a8
Xavier Muller <xavier.muller@etu.u-bordeaux.fr>
2021-01-02 16:41:40 +0100

    remove debug info

--- cmake/avr.toolchain.cmake
+++ cmake/avr.toolchain.cmake
@@ -79,8 +79,6 @@ if(NOT _ATMEGA328P_BUILD_SET)
 #  → GENERATE_BINARY\n\
 #  → GENERATE_FLASH_CMD\n\
 #    ")
-    message(STATUS ${target_name})
-    message(STATUS ${ARGN})
     add_executable(${target_name} ${ARGN})
 #    set(options
 #       GENERATE_BINARY
